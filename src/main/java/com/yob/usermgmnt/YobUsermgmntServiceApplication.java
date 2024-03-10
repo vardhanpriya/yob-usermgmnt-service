a49fbf94a7b6a2e780c074d3eb26a05c9f832c29
Kirti Vardhan <kirtivardhan8@gmail.com>
2024-03-11 02:04:18 +0530

    Project created

---
+++ src/main/java/com/yob/usermgmnt/YobUsermgmntServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yob.usermgmnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YobUsermgmntServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YobUsermgmntServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yob/usermgmnt/YobUsermgmntServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yob.usermgmnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YobUsermgmntServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
